a183c2fc5c34ece1a767000808c6035c5e77fc22
brandon-upp <bupp@discovernewfields.org>
2025-09-25 13:25:31 -0400

    added icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Hello World. <Link href='/about'>About</Link>
+      <div>
+        <Image
+          aria-hidden
+          src='/next-static/globe.svg'
+          alt='Globe icon'
+          width={16}
+          height={16}
+        />
+      </div>
     </div>
   );
 }
